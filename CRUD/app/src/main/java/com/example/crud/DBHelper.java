87a2518b208ae4332f40ed57c0c5cd6530984d0c
BSEF19M513 <bsef19m513@pucit.edu.pk>
2022-08-19 14:43:18 -0700

    CRUD APP

---
+++ CRUD/app/src/main/java/com/example/crud/DBHelper.java
@@ -0,0 +1,94 @@
+package com.example.crud;
+
+import android.app.Application;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String tableStatement = "Create Table Records (NAME Text, ID Text,SECTION Text)";
+        sqLiteDatabase.execSQL(tableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists Records");
+    }
+
+    public DBHelper(Context context)
+    {
+        super(context, "MyDB.db", null, 1);
+    }
+
+    public Boolean insertRecord(String name, String id, String section)
+    {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("NAME", name);
+        values.put("ID", id);
+        values.put("SECTION", section);
+
+        long res = database.insert("Records", null, values);
+        if (res==-1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public Cursor viewRecords()
+    {
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery("Select * from Records", null);
+        return cursor;
+    }
+
+    public Boolean updateRecord(String name, String id, String section)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NAME", name);
+        contentValues.put("ID", id);
+        contentValues.put("SECTION", section);
+        Cursor cursor = DB.rawQuery("Select * from Records where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.update("Records", contentValues, "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean deleteRecord (String name)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Records where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.delete("Records", "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
---
+++ CRUD/app/src/main/java/com/example/crud/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText name, id, section;
+        Button add, view, update, delete;
+        DBHelper helper;
+        name = findViewById(R.id.name);
+        id  = findViewById(R.id.id);
+        section = findViewById(R.id.section);
+
+        add = findViewById(R.id.add);
+        view = findViewById(R.id.view);
+        update = findViewById(R.id.update);
+        delete = findViewById(R.id.delete);
+        helper = new DBHelper(this);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name_txt = name.getText().toString();
+                String id_txt = id.getText().toString();
+                String section_txt = section.getText().toString();
+                Boolean checkInsertData = helper.insertRecord(name_txt, id_txt, section_txt);
+                if (checkInsertData)
+                    Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_SHORT).show();
+                helper.close();
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = helper.viewRecords();
+                if(res.getCount()==0){
+                    Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Name: "+res.getString(0)+"\n");
+                    buffer.append("Student ID: " + res.getString(1) + "\n" );
+                    buffer.append("Section: "+res.getString(2)+"\n");
+                }
+
+                //alert pop-up for viewing all data
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(buffer.toString());
+                builder.show();
+
+                helper.close();
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name_txt = name.getText().toString();
+                String id_txt = id.getText().toString();
+                String section_txt = section.getText().toString();
+
+                Boolean checkUpdateData = helper.updateRecord(name_txt, id_txt, section_txt);
+                if(checkUpdateData)
+                    Toast.makeText(MainActivity.this, "Entry Updated", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "New Entry Not Updated", Toast.LENGTH_SHORT).show();
+                helper.close();
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name_txt = name.getText().toString();
+                Boolean checkDeleteData = helper.deleteRecord(name_txt);
+                if(checkDeleteData)
+                    Toast.makeText(MainActivity.this, "Entry Deleted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Entry Not Deleted", Toast.LENGTH_SHORT).show();
+
+                helper.close();
+            }
+        });
+
+    }
+
+}
